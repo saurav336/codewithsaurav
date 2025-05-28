79a8be0298548140e27386dfb8c7e15b11b8cc81
sauravmalekoo <sauravmalekoo@192.168.1.11>
2025-05-28 21:47:03 +0545

    merge issued fixed

--- public/index.php
+++ public/index.php
@@ -4,3 +4,10 @@ declare(strict_types=1);
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
+function sum(int|float ...$numbers): int|float {
+    return array_sum($numbers);
+}
+
+$x = 'sum';
+
+echo $x(1, 2, 3); // Outputs: 6
