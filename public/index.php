34d0768d48180f24106cce54a8c6d45786acd922
sauravmalekoo <sauravmalekoo@192.168.1.11>
2025-05-28 17:22:31 +0545

    removed src folder

--- public/index.php
+++ public/index.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-function sum(int|float ...$numbers): int|float {
-    return array_sum($numbers);
+function sum(int|float ...$numbers) : int|float {
+    $sum = 0;
+    foreach ($numbers as $number) {
+        $sum += $number;
+        
+ 
+   }
+   return $sum;
 }
 
-$x = 'sum';
-
-echo $x(1, 2, 3); // Outputs: 6
+echo sum(1, 10,55,88) . "\n";
